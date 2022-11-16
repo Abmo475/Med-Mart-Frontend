cba0959889dadf8fed413abe25d3282128a1d1bb
Abdulkadir Mohammed <abmo475@gmail.com>
2022-11-16 15:34:51 +0300

    alhamdililah

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -2,12 +2,16 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Searchbar from './Searchbar';
-import applogo from '../../../assets/images/logo.png';
+import logo from '../../../assets/images/logo.png';
 import PrimaryDropDownMenu from './PrimaryDropDownMenu';
 import SecondaryDropDownMenu from './SecondaryDropDownMenu';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import ImportExportIcon from '@mui/icons-material/ImportExport';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import Groups3Icon from '@mui/icons-material/Groups';
 
 const Header = () => {
 
@@ -20,15 +24,15 @@ const Header = () => {
 
   return (
 
-    <header className="bg-primary-blue fixed top-0 py-2.5 w-full z-10">
+    <header className="bg-primary-orange fixed top-0 py-2.5 w-full z-10">
 
       {/* <!-- navbar container --> */}
-      <div className="w-full sm:w-9/12 px-1 sm:px-4 m-auto flex justify-between items-center relative">
+      <div className="w-full sm:w-8/11 px-1 sm:px-4 m-auto flex justify-between items-center relative">
 
         {/* <!-- logo & search container --> */}
         <div className="flex items-center flex-1">
-          <Link className="h-7 mr-1 sm:mr-4" to="/">
-            <img draggable="false" className="h-full w-full object-contain" src={applogo} alt="Flipkart Logo" />
+          <Link className="h-12 mr-1 sm:mr-5" to="/">
+            <img draggable="false" className="h-full w-full object-contain" src={logo} alt="Flipkart Logo" />
           </Link>
 
           <Searchbar />
@@ -36,10 +40,38 @@ const Header = () => {
         {/* <!-- logo & search container --> */}
 
         {/* <!-- right navs --> */}
-        <div className="flex items-center justify-between ml-1 sm:ml-0 gap-0.5 sm:gap-7 relative">
+        <span className="moreDropDown hidden sm:flex px-3 items-center text-white font-medium gap-1 cursor-pointer">Bidding
+        <span><ReceiptIcon /></span>
+        </span>
+        <span className="moreDropDown hidden  sm:flex px-3 items-center text-white font-medium gap-1 cursor-pointer">
+          Importers
+          <span><ImportExportIcon /></span>
+          </span>
+     
+        
+        <span className="moreDropDown hidden sm:flex px-3 items-center text-white font-medium gap-2 cursor-pointer">
+          Products
+            <span><InventoryIcon sx={{ fontSize: "16px" }} /> </span>
+          </span>
+          <span className="moreDropDown hidden sm:flex px-3 items-center text-white font-medium gap-2 cursor-pointer">
+          clique
+            <span><Groups3Icon sx={{ fontSize: "22px" }} /> </span>
+          </span>
+        <div className="flex items-center  px-3  justify-between ml-1 sm:ml-0 gap-0.5 sm:gap-7 relative">
 
-          {isAuthenticated === false ?
-            <Link to="/login" className="px-3 sm:px-9 py-0.5 text-primary-blue bg-white border font-medium rounded-sm cursor-pointer">Login</Link>
+
+          <Link to="/cart" className="flex items-center text-white font-medium gap-2 relative">
+            <span><ShoppingCartIcon /></span>
+            {cartItems.length > 0 &&
+              <div className="w-5 h-5 p-2 bg-red-500 text-xs rounded-full absolute -top-2 left-3 flex justify-center items-center border">
+                {cartItems.length}
+              </div>
+            }
+            My Folder
+          </Link>
+        </div>
+        {isAuthenticated === false ?
+            <Link to="/login" className="px-3 sm:px-9 py-0.5 text-white  font-medium rounded-sm cursor-pointer">Login</Link>
             :
             (
               <span className="userDropDown flex items-center text-white font-medium gap-1 cursor-pointer" onClick={() => setTogglePrimaryDropDown(!togglePrimaryDropDown)}>{user.name && user.name.split(" ", 1)}
@@ -50,22 +82,6 @@ const Header = () => {
 
           {togglePrimaryDropDown && <PrimaryDropDownMenu setTogglePrimaryDropDown={setTogglePrimaryDropDown} user={user} />}
 
-          <span className="moreDropDown hidden sm:flex items-center text-white font-medium gap-1 cursor-pointer" onClick={() => setToggleSecondaryDropDown(!toggleSecondaryDropDown)}>More
-            <span>{toggleSecondaryDropDown ? <ExpandLessIcon sx={{ fontSize: "16px" }} /> : <ExpandMoreIcon sx={{ fontSize: "16px" }} />}</span>
-          </span>
-
-          {toggleSecondaryDropDown && <SecondaryDropDownMenu />}
-
-          <Link to="/cart" className="flex items-center text-white font-medium gap-2 relative">
-            <span><ShoppingCartIcon /></span>
-            {cartItems.length > 0 &&
-              <div className="w-5 h-5 p-2 bg-red-500 text-xs rounded-full absolute -top-2 left-3 flex justify-center items-center border">
-                {cartItems.length}
-              </div>
-            }
-            Cart
-          </Link>
-        </div>
         {/* <!-- right navs --> */}
 
       </div>

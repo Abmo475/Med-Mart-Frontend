41990ac94b2837d952b2e1bfd67c7d47ebbb34aa
Abdulkadir Mohammed <abmo475@gmail.com>
2022-11-17 14:22:46 +0300

    homepage

--- src/components/Layouts/Header/PrimaryDropDownMenu.jsx
+++ src/components/Layouts/Header/PrimaryDropDownMenu.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import OfflineBoltIcon from '@mui/icons-material/OfflineBolt';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ChatIcon from '@mui/icons-material/Chat';
-import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
-import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import DashboardIcon from '@mui/icons-material/Dashboard';
@@ -32,13 +29,8 @@ const PrimaryDropDownMenu = ({ setTogglePrimaryDropDown, user }) => {
 
     const navs = [
         {
-            title: "Supercoin Zone",
+            title: "Hospitals",
             icon: <OfflineBoltIcon sx={{ fontSize: "18px" }} />,
-            redirect: "/",
-        },
-        {
-            title: "Flipkart Plus Zone",
-            icon: <AddCircleIcon sx={{ fontSize: "18px" }} />,
             redirect: "/",
         },
         {
@@ -56,16 +48,7 @@ const PrimaryDropDownMenu = ({ setTogglePrimaryDropDown, user }) => {
             icon: <ChatIcon sx={{ fontSize: "18px" }} />,
             redirect: "/",
         },
-        {
-            title: "Coupons",
-            icon: <ConfirmationNumberIcon sx={{ fontSize: "18px" }} />,
-            redirect: "/",
-        },
-        {
-            title: "Gift Cards",
-            icon: <AccountBalanceWalletIcon sx={{ fontSize: "18px" }} />,
-            redirect: "/",
-        },
+    
         {
             title: "Notifications",
             icon: <NotificationsIcon sx={{ fontSize: "18px" }} />,
@@ -74,7 +57,7 @@ const PrimaryDropDownMenu = ({ setTogglePrimaryDropDown, user }) => {
     ]
 
     return (
-        <div className="absolute w-60 -left-24 ml-2 top-9 bg-white shadow-2xl rounded flex-col text-sm">
+        <div className="absolute w-60 -right-16 ml-2 top-9 bg-white shadow-2xl rounded flex-col text-sm">
 
             {user.role === "admin" &&
                 <Link className="pl-3 py-3.5 border-b flex gap-3 items-center hover:bg-gray-50 rounded-t" to="/admin/dashboard">

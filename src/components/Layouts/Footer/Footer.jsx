99e91b44d0b57abf934f6ddc409aed373fc4a849
Abdulkadir Mohammed <abmo475@gmail.com>
2022-11-18 10:54:35 +0300

    json

--- src/components/Layouts/Footer/Footer.jsx
+++ src/components/Layouts/Footer/Footer.jsx
@@ -12,31 +12,31 @@ const footerLinks = [
     links: [
       {
         name: "Contact Us",
-        redirect: "https://www.flipkart.com/helpcentre",
+        redirect: "https://www.medmart.com/helpcentre",
       },
       {
         name: "About Us",
-        redirect: "https://www.flipkart.com/about-us",
+        redirect: "https://www.medmart.com/about-us",
       },
       {
         name: "Careers",
-        redirect: "https://www.flipkartcareers.com",
+        redirect: "https://www.medmartcareers.com",
       },
       {
-        name: "Flipkart Stories",
-        redirect: "https://stories.flipkart.com",
+        name: "medmart Stories",
+        redirect: "https://stories.medmart.com",
       },
       {
         name: "Press",
-        redirect: "https://stories.flipkart.com/category/top-stories/news",
+        redirect: "https://stories.medmart.com/category/top-stories/news",
       },
       {
-        name: "Flipkart Wholesale",
-        redirect: "https://www.flipkartwholesale.com",
+        name: "medmart Wholesale",
+        redirect: "https://www.medmartwholesale.com",
       },
       {
         name: "Corporate Information",
-        redirect: "https://www.flipkart.com/corporate-information",
+        redirect: "https://www.medmart.com/corporate-information",
       },
     ]
   },
@@ -45,19 +45,19 @@ const footerLinks = [
     links: [
       {
         name: "Payments",
-        redirect: "https://www.flipkart.com/pages/payments",
+        redirect: "https://www.medmart.com/pages/payments",
       },
       {
         name: "Shipping",
-        redirect: "https://www.flipkart.com/pages/shipping",
+        redirect: "https://www.medmart.com/pages/shipping",
       },
       {
         name: "Cancellation & Returns",
-        redirect: "https://www.flipkart.com/helpcentre?catalog=55c9c6edb000002e002c1701&view=CATALOG",
+        redirect: "https://www.medmart.com/helpcentre?catalog=55c9c6edb000002e002c1701&view=CATALOG",
       },
       {
         name: "FAQ",
-        redirect: "https://www.flipkart.com/helpcentre?catalog=55c9c8e2b0000023002c1702&view=CATALOG",
+        redirect: "https://www.medmart.com/helpcentre?catalog=55c9c8e2b0000023002c1702&view=CATALOG",
       }
     ]
   },
@@ -66,27 +66,27 @@ const footerLinks = [
     links: [
       {
         name: "Return Policy",
-        redirect: "https://www.flipkart.com/pages/returnpolicy",
+        redirect: "https://www.medmart.com/pages/returnpolicy",
       },
       {
         name: "Terms Of Use",
-        redirect: "https://www.flipkart.com/pages/terms",
+        redirect: "https://www.medmart.com/pages/terms",
       },
       {
         name: "Security",
-        redirect: "https://www.flipkart.com/pages/paymentsecurity",
+        redirect: "https://www.medmart.com/pages/paymentsecurity",
       },
       {
         name: "Privacy",
-        redirect: "https://www.flipkart.com/pages/privacypolicy",
+        redirect: "https://www.medmart.com/pages/privacypolicy",
       },
       {
         name: "Sitemap",
-        redirect: "https://www.flipkart.com/sitemap",
+        redirect: "https://www.medmart.com/sitemap",
       },
       {
         name: "EPR Compliance",
-        redirect: "https://www.flipkart.com/pages/ewaste-compliance-tnc",
+        redirect: "https://www.medmart.com/pages/ewaste-compliance-tnc",
       },
     ]
   },
@@ -95,15 +95,15 @@ const footerLinks = [
     links: [
       {
         name: "Facebook",
-        redirect: "https://www.facebook.com/flipkart",
+        redirect: "https://www.facebook.com/medmart",
       },
       {
         name: "Twitter",
-        redirect: "https://twitter.com/flipkart",
+        redirect: "https://twitter.com/medmart",
       },
       {
         name: "YouTube",
-        redirect: "https://www.youtube.com/flipkart",
+        redirect: "https://www.youtube.com/medmart",
       }
     ]
   }
@@ -141,25 +141,26 @@ const Footer = () => {
             <div className="w-full sm:w-5/12 my-6 mx-5 sm:mx-0 flex flex-col sm:flex-row gap-2 sm:gap-0 justify-between">
               <div className="w-full sm:w-1/2">
                 <h2 className="text-primary-grey">Mail Us:</h2>
-                <p className="mt-2 leading-5">Flipkart Internet Private Limited,<br />
-                  Buildings Alyssa, Begonia &<br />
-                  Clove Embassy Tech Village,<br />
-                  Outer Ring Road, Devarabeesanahalli Village,<br />
-                  Bengaluru, 560103,<br />
-                  Karnataka, India
+                <p className="mt-2 leading-5">Medmart Private Limited,<br />
+                Building TBA &<br />
+                Address line 1 TBA,<br />
+                Address line 2 TBA,<br />
+                Subcity TBA, POBOX TBA,<br />
+                Addis Ababa, Ethiopia<br />
+                Email: TBA
                 </p>
               </div>
 
               <div className="w-full sm:w-1/2">
                 <h2 className="text-primary-grey">Registered Office Address:</h2>
-                <p className="mt-2 leading-5">Flipkart Internet Private Limited,<br />
-                  Buildings Alyssa, Begonia &<br />
-                  Clove Embassy Tech Village,<br />
-                  Outer Ring Road, Devarabeesanahalli Village,<br />
-                  Bengaluru, 560103,<br />
-                  Karnataka, India <br />
-                  CIN : U51109KA2012PTC066107<br />
-                  Telephone: <a className="text-primary-blue" href="tel:18002029898">1800 202 9898</a>
+                <p className="mt-2 leading-5">Medmart Private Limited,<br />
+                  Building TBA &<br />
+                  Address line 1 TBA,<br />
+                  Address line 2 TBA,<br />
+                  Subcity TBA, POBOX TBA,<br />
+                  Addis ABaba, Ethiopia <br />
+                
+                  Telephone: <a className="text-primary-blue" href="tel:911">xx xx xx xx xx</a>
                 </p>
               </div>
             </div>
@@ -168,20 +169,20 @@ const Footer = () => {
           {/* <!-- footer ends --> */}
 
           <div className="px-16 py-6 w-full bg-primary-darkBlue hidden sm:flex justify-between items-center text-sm text-white">
-            <a href="https://seller.flipkart.com/sell-online" target="_blank" rel="noreferrer" className="flex items-center gap-2">
-              <span className="text-yellow-400"><WorkIcon sx={{ fontSize: "20px" }} /></span> Sell On Flipkart
+            <a href="https://seller.medmart.com/sell-online" target="_blank" rel="noreferrer" className="flex items-center gap-2">
+              <span className="text-yellow-400"><WorkIcon sx={{ fontSize: "20px" }} /></span>Sell On MedMart
             </a>
-            <a href="https://brands.flipkart.com" target="_blank" rel="noreferrer" className="flex items-center gap-2">
+            <a href="https://brands.medmart.com" target="_blank" rel="noreferrer" className="flex items-center gap-2">
               <span className="text-yellow-400"><StarsIcon sx={{ fontSize: "20px" }} /></span> Advertise
             </a>
-            <a href="https://www.flipkart.com/the-gift-card-store" rel="noreferrer" target="_blank" className="flex items-center gap-2">
-              <span className="text-yellow-400"><CardGiftcardIcon sx={{ fontSize: "20px" }} /></span> Gift Cards
+            <a href="https://www.medmart.com/the-gift-card-store" rel="noreferrer" target="_blank" className="flex items-center gap-2">
+              <span className="text-yellow-400"><CardGiftcardIcon sx={{ fontSize: "20px" }} /></span> Package 
             </a>
-            <a href="https://www.flipkart.com/helpcentre" target="_blank" rel="noreferrer" className="flex items-center gap-2">
+            <a href="https://www.medmart.com/helpcentre" target="_blank" rel="noreferrer" className="flex items-center gap-2">
               <span className="text-yellow-400"><HelpIcon sx={{ fontSize: "20px" }} /></span> Help Center
             </a>
 
-            <span>&copy; 2007-{new Date().getFullYear()} Flipkart.com</span>
+            <span>&copy; 2007-{new Date().getFullYear()} Medmart.com</span>
             <img draggable="false" src={paymentMethods} alt="Card Payment" />
           </div>
         </>
